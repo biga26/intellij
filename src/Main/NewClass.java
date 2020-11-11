2048e01633e35a71e009ff25b44fd39e4382a585
biga26 <amanmuradov.begench@gmail.com>
2020-11-11 17:36:47 -0500

    Created from INtellij again

---
+++ src/Main/NewClass.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("IntellIJ");
+        System.out.println("Hello");
+
+
+    }
+
+}
